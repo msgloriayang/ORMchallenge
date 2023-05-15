9e640912a4a6eaf7a3fc9aeae5c219a4ca46ffaf
msgloriayang <yanggloria91@gmail.com>
2023-05-15 01:13:35 -0500

    added Product.findAll and Category.findOne in product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,58 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_id' ]
+    },
+  {
+    model: Tag,
+    attribute: ['id', 'tag_name'],
+  },
+],
+  })
+  .then(categoryData => {
+    if(!categoryData) {
+      res.status(404).json({message: "Category does not exist"});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Category,
+      attributes: ['id', 'category_id'],
+    },
+    {
+      model: Tag,
+      attribute: ['id', 'tag_name'],
+    },
+  })
+  .then((categoryData) => {
+    if (!categoryData) {
+      res.status(404).json({message: "Category does not exist"});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
