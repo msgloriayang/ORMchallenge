43c450ab0a89d9e43e34032116246cfc1c90cbab
msgloriayang <yanggloria91@gmail.com>
2023-05-15 00:54:11 -0500

    defined columns for ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: 'id',
+        model: 'tag',
+      }
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: 'id',
+        model: 'product',
+      }
+    }
   },
   {
     sequelize,
