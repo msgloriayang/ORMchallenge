787882ec25f463b4af3b4e35a0d961b64d018a05
msgloriayang <yanggloria91@gmail.com>
2023-05-15 00:48:24 -0500

    added fields and rules for product model in product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,26 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+        }
+      }
+    }
     // define columns
   },
   {
