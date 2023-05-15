7bcc7b03c53d53025c230161703339eed1a0c377
msgloriayang <yanggloria91@gmail.com>
2023-05-15 00:35:18 -0500

    added id and category names in category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // id
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    // category names
+    categoryNames: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
